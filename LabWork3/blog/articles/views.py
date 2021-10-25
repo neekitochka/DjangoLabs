ff3cab01b98b43d61cc47a2d0a14b0690e612075
Nikita Kochrinskiy <nik.koch.67@yandex.ru>
2021-10-25 18:42:08 +0300

    Laboratory Work 3 is done

---
+++ LabWork3/blog/articles/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from articles.models import Article
+
+
+def archive(request):
+    return render(request, 'templates/archive.html', {"posts": Article.objects.all()})
